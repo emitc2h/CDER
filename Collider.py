de0b25413faaafe10876a01ea6dba146af21b5af
Michel Trottier-McDonald <mta58@sfu.ca>
2012-12-19 16:15:39 -0500

    basic particle model

--- Collider.py
+++ Collider.py
@@ -10,7 +10,15 @@ from core.particle import particle
 particles = [particle.Particle(50000, 1.2, 0.8, 1, (0.0, 0.7, 0.7)),
              particle.Particle(10000, 0.6, -2.4, 1, (0.7, 0.7, 0.0)),
              particle.Particle(160000, -0.4, 2.4, 1, (0.9, 0.1, 0.0)),
-             particle.Particle(100000, -1.4, -1.2, 1, (0.1, 0.1, 0.9))]
+             particle.Particle(100000, -1.4, -1.2, 1, (0.1, 0.1, 0.9)),
+             particle.Particle(50000, 1.3, 0.8, 1, (0.0, 0.7, 0.7)),
+             particle.Particle(10000, 0.7, -2.4, 1, (0.7, 0.7, 0.0)),
+             particle.Particle(160000, -0.5, 2.4, 1, (0.9, 0.1, 0.0)),
+             particle.Particle(100000, -1.5, -1.2, 1, (0.1, 0.1, 0.9)),
+             particle.Particle(50000, 1.4, 0.9, 1, (0.0, 0.7, 0.7)),
+             particle.Particle(10000, 0.8, -2.5, 1, (0.7, 0.7, 0.0)),
+             particle.Particle(160000, -0.6, 2.5, 1, (0.9, 0.1, 0.0)),
+             particle.Particle(100000, -1.6, -1.3, 1, (0.1, 0.1, 0.9))]
 
 from core.particle import beamline
 beam = beamline.Beamline(particles)
