911e748aa240cc4f022560be3a61b0395f51bd52
Michel Trottier-McDonald <mta58@sfu.ca>
2012-12-27 01:37:55 -0500

    Cleaned addVectorToROOT.C

--- core/reader/addVectorToROOT.C
+++ core/reader/addVectorToROOT.C
@@ -1,4 +1,12 @@
-// File loader.C
+//*****************************************//
+// file   : addVectorToROOT.C              //
+// author : Dugan O'Neil                   //
+// date   : 2009                           //
+// description:                            //
+// Add vectors of floats, ints and ROOT    //
+// TLorentzVectors to ROOT dictionary      //
+//*****************************************//
+
 #include <vector>
 #include "TLorentzVector.h"
 #ifdef __MAKECINT__
